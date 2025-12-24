1242224f15a4462b6747845f6facb0c5830e8c0a
vishvajeet <djritikshukla@gmail.com>
2025-12-24 16:06:20 +0530

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <main className=" w-full bg-warm-cream  overflow-hidden">
       <Hero/>
-      <DeconstructedGalleryHero/>
+      {/* <DeconstructedGalleryHero/> */}
       <AnnouncementHomepage />
 
       <TopCategory/>
